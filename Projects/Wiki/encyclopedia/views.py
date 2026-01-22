cb4f5b784d1fa0491698e4a30ff2c5d80a9d1400
Alex Pantis <pantisalex921@yahoo.com>
2026-01-22 22:24:15 +0200

    wiki project

--- Projects/Wiki/encyclopedia/views.py
+++ Projects/Wiki/encyclopedia/views.py
@@ -5,3 +5,19 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    content = util.get_entry(title)
+    
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": f"Page '{title}' not found"
+        })
+    
+    import markdown2
+    html_content = markdown2.markdown(content)
+    
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": html_content
+    })
